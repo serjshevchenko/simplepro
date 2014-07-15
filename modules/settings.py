1a08401748547a735aa4060d6b91cec44e96e905
Serj Shevchenko <serj.shevchenko@gmail.com>
2014-07-15 15:10:08 +0300

    deleted pycache file

--- modules/settings.py
+++ modules/settings.py
@@ -2,12 +2,16 @@
 
 import cherrypy
 
+from jinja2 import Environment, PackageLoader
+env = Environment(loader=PackageLoader('public', 'templates'))
+
 class Settings (object) :
     
     @cherrypy.expose
     def index (self) :
-        return 'settigns info...'
+        temp = env.get_template('pages/settings/index.html')
+        return temp.render(message='Settings')
     
-    @cherrypy.expose    
+    @cherrypy.expose
     def edit (self, id) :
         return 'Edit info of record # %s' % (id or 0);
